fa9d72659d7a295d730168b53420e050c6dc8b87
Sourav Saini <souravsaini@gmail.com>
2023-11-23 16:14:40 -0500

    Chat system code (client + server)

---
+++ client/client.js
@@ -0,0 +1,19 @@
+const io = require("socket.io-client");
+
+const socket = io("http://localhost:3000");
+
+socket.on("connect", () => {
+  console.log("Connected to server");
+  sendMessage(socket.id);
+});
+
+socket.on("chat message", (msg) => {
+  console.log("Received message:", msg);
+});
+
+function sendMessage(id) {
+  setInterval(() => {
+    const message = `Hello from client: ${id}`;
+    socket.emit("chat message", message);
+  }, 2000);
+}
